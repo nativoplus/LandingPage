7c8067f58f2eddafd9740a89602ae6ca7bea3e5a
Hector <oreyes.hec@gmail.com>
2022-01-19 16:34:55 -0400

    Services, Team, Experience Pages

---
+++ LandingPage/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace LandingPage.Models
+{
+    public class Article
+    {
+        public string Title { get; set; } = "";
+        public List<string> Bullets { get; set; } = new List<string> { };
+        public string PhotoUrl { get; set; } = "";
+    }
+}
---
+++ LandingPage/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace LandingPage.Models
+{
+    public class Team
+    {
+        public string Name { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string PhotoUrl { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string GithubUrl { get; set; } = "";
+        public string LinkedIn { get; set; } = "";
+
+    }
+}
