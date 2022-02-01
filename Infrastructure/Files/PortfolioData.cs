2dc130a4d76d634c2c18928327cbea04cecaaed2
Hector <oreyes.hec@gmail.com>
2022-02-01 16:23:43 -0400

    Updated Fontsize, Added Fertilemind Page

--- Infrastructure/Files/PortfolioData.cs
+++ Infrastructure/Files/PortfolioData.cs
@@ -86,6 +86,69 @@ namespace LandingPage.Infrastructure.Files
                         GithubUrl = "",
                     },
                 }
+            },
+            new Portfolio
+            {
+                ProjectId = 0,
+                Name = "Fertilemind",
+                // Uses String Literal to make the string more readable on development. 
+                Headline=@"Fertilemind is an app that approaches infertility by intentional
+                mental activity improving reproductive health cycles. It combines
+                guidance from the Award Winning FertileMind Program with an
+                interface to make your fertility journey effective and stress-free.",
+                //Managing Body as a Markup string to be able to generate body more dynamically. 
+                Body = new MarkupString(@"<p>Fertilemind needed an App focused on UX and a clean and easyto-
+                use platform. We develop a user interface to showcase its
+                apps and provide a tool for its users to interact with the app. We
+                decided to build a UI based on the React framework, which was
+                already being used in the backend.</p>"),
+                ImgUrls = new List<string>{
+                    "template/images/assets/fm/fertilemind.png",
+                    //"template/images/assets/gb/templates.jpg",
+                },
+                Services = new List<string>
+                {
+                    "Web development",
+                    "Web Design",
+                    "Social Media Managment",
+                    "Mobile App Redesign",
+                    "API Development",
+                    "Database Design",
+                    "Server Management",
+                    "Deployment Automation"
+                },
+                Technologies = new List<string>
+                {
+                    "Flutter / Dart",
+                    "Firebase",
+                    "SQL Server"
+
+                },
+                Practices = new List<string>
+                {
+                    "User Research",
+                    "UX Design",
+                    "Agile Development",
+                    "Restful API",
+                    "Data Management",
+                    "Technical Discovery",
+                    "Mobile Design",
+                    "Product Management",
+                    "Cloud Hosting",
+                    "Open Source Development"
+                },
+                Team = new List<Team>
+                {
+                    new Team
+                    {
+                        Name = "Arnaldo Rodriguez",
+                        Title = "Software Engineer",
+                        Description = "Lorem Ipsum",
+                        ImgUrl = "template/images/team/arnaldo.jpg",
+                        LinkedIn = "",
+                        GithubUrl = "",
+                    },
+                }
             }
         };
     }
