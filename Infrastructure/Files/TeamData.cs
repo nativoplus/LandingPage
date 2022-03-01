a06d7ee664ac04f1398ef56af87f0ea8f045d1d5
Hector <oreyes.hec@gmail.com>
2022-03-01 17:56:32 -0400

    Added Modal Support, Started Team Details Page, Added Images, Fix Modal Javascript Initializer, Added Modal Component

--- Infrastructure/Files/TeamData.cs
+++ Infrastructure/Files/TeamData.cs
@@ -15,7 +15,7 @@ namespace LandingPage.Infrastructure.Files
                 Name = "Hector Orellana Reyes",
                 Role = "Software Engineer",
                 Description = @"Hector has been a UI Developer with Nativo Plus Studio since January 2017.",
-                ImgUrl = "template/images/team/hector.jpg",
+                ImgUrl = "template/images/team/hector.png",
                 LinkedIn = "https://www.linkedin.com/in/hector-orellana-reyes-220b7a113/",
                 GithubUrl = "https://github.com/horeyes",
                 Projects = new List<string>{ "Gentlebirth" },
@@ -28,7 +28,7 @@ namespace LandingPage.Infrastructure.Files
                 based in the San Francico Bay Area, he has over 10 years of experience with ovreseing 
                 the application development lifecycle from conception to implementation as a senior 
                 software engineer and software architect in the public and private sectors.",
-                ImgUrl = "template/images/team/jose.jpg",
+                ImgUrl = "template/images/team/jose.png",
                 LinkedIn = "https://www.linkedin.com/in/joecolon/",
                 GithubUrl = "https://github.com/jvelezc",
                 Projects = new List<string>{ "TechCU", "Gentlebirth", "Fertilemind" },
@@ -38,7 +38,7 @@ namespace LandingPage.Infrastructure.Files
                 Name = "Angel Ramos",
                 Role = "Software Engineer",
                 Description = @"Angel has been a Software Engineer with Nativo Plus Studio since April 2017.  ",
-                ImgUrl = "template/images/team/ramos.jpg",
+                ImgUrl = "template/images/team/ramos.png",
                 LinkedIn = "https://www.linkedin.com/in/angel-ramos-5a6a52a2/",
                 GithubUrl = "https://github.com/joey0xx",
                 Projects = new List<string>{ "TechCU"},
@@ -46,11 +46,11 @@ namespace LandingPage.Infrastructure.Files
             new Team
             {
                 Name = "Arnaldo Rodriguez",
-                Role = "Full Stack Developer",
-                Description = @"Arnaldo has been a Full Stack Developer with Nativo Plus Studio since December 2016.  
-                Based in Puerto Rico, Arnaldo has a broad range of experience with designing and developing web 
-                applications using AngularJS, C#, and TypeScript.  ",
-                ImgUrl = "template/images/team/arnaldo.jpg",
+                Role = "Software Engineer",
+                Description = @"Focused and passionate about creating quality products, that involve a great 
+                design as well as great code. He likes to follow the best practices and latest design patterns. 
+                Arnaldo is a professional that promotes and empowers creativity and a healthy work environment.",
+                ImgUrl = "template/images/team/arnaldo.png",
                 LinkedIn = "https://www.linkedin.com/in/arnaldo-rodriguez-1445b811b/",
                 GithubUrl = "https://github.com/arodriguezgb",
                 Projects = new List<string>{ "Gentlebirth", "Fertilemind" },
@@ -65,7 +65,7 @@ namespace LandingPage.Infrastructure.Files
                 ImgUrl = "template/images/team/amaro.png",
                 LinkedIn = "https://www.linkedin.com/in/cristian-amaro-burgos-64815816a/",
                 GithubUrl = "https://github.com/camaro24max",
-                Projects = new List<string>{ "Gentlebirth", "Fertilemind" },
+                Projects = new List<string>{ },
             },
             new Team
             {
@@ -77,7 +77,7 @@ namespace LandingPage.Infrastructure.Files
                 ImgUrl = "template/images/team/marie.png",
                 LinkedIn = "https://www.linkedin.com/in/mariefitch/",
                 GithubUrl = "",
-                Projects = new List<string>{ "Gentlebirth", "Fertilemind" },
+                Projects = new List<string>{  },
             },
             new Team
             {
@@ -112,7 +112,7 @@ namespace LandingPage.Infrastructure.Files
                 asked. He is focused on software and web application development, with a high level of creativity, 
                 a positive attitude, and a desire to learn and improve day by day. He is friendly and professional 
                 approach has resulted in many successful projects for clients of all sizes.",
-                ImgUrl = "template/images/team/danny.jfif",
+                ImgUrl = "template/images/team/danny.png",
                 LinkedIn = "https://www.linkedin.com/in/danny-stev-ruiz/",
                 GithubUrl = "",
                 Projects = new List<string>{ },
@@ -126,6 +126,20 @@ namespace LandingPage.Infrastructure.Files
                 Sales Specialist, she has a record of success in boosting brands through tailored strategies.",
                 ImgUrl = "template/images/team/sara.png",
                 LinkedIn = "https://www.linkedin.com/in/saracabrerao/",
+                GithubUrl = "",
+                Projects = new List<string>{ },
+            },
+            new Team
+            {
+                Name = "Bradley Bailey-Murray",
+                Role = "Business Development",
+                Description = @"Taking concepts and turning them into reality through design and code is what 
+                gets him up in the morning. Having the opportunity to utilize his abilities to solve issues 
+                and constraints through planning and strategy drives him to continue to learn and grow as a 
+                designer and programmer on a daily basis. He loves to watch projects come to life through the 
+                product cycle.",
+                ImgUrl = "template/images/team/bradley.png",
+                LinkedIn = "https://www.linkedin.com/in/bradley-bailey-murray-3823a053/",
                 GithubUrl = "",
                 Projects = new List<string>{ },
             }
